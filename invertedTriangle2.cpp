f1b34e93fa7551e58ddcaa58710d5ef74049b22f
IshaanSaxena2005 <saxenaishaan3@gmail.com>
2026-01-29 20:29:41 +0530

    Add files via upload

---
+++ invertedTriangle2.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n=4;
+    char alpha='A';
+    for(int i=0;i<n;i++){
+        for(int j=0;j<i;j++){
+            cout<<" ";
+        }
+        for(int j=0;j<n-i;j++){
+            cout<<alpha;
+        }
+    cout<<"\n";
+    alpha++;
+    }
+    
+    return 0;
+}
