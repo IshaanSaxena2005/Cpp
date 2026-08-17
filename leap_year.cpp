038c408d0c8ed775eedba3454421f557f3fc9b94
Ishaan Saxena <saxenaishaan3@gmail.com>
2026-08-18 00:18:28 +0530

    Add files via upload

---
+++ leap_year.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int year;
+    cin>>year;
+    if((year%400==0)||(year%4==0 && year%100!=0))
+        cout<<"Leap";
+    else
+        cout<<"Not Leap";
+    return 0;
+}
